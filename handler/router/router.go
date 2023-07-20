82a70b5e295d40d2b13ed77a564a54196cc343ac
Naga564 <yanagayakisoba@gmail.com>
2023-07-20 21:55:27 +0900

    fix Endpoint

--- handler/router/router.go
+++ handler/router/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
@@ -14,7 +15,7 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux.Handle("/healthz", handler.NewHealthzHandler())
 
 	//todo
-	mux.Handle("http://localhost:8080/todos", handler.NewTODOHandler())
+	mux.Handle("http://localhost:8080/todos", handler.NewTODOHandler(service.NewTODOService(todoDB)))
 
 	return mux
 }
